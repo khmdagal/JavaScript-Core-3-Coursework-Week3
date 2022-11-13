d22e202f40bcb5b42d102e3be2f490c2a0794929
khmdagal <khmdagal@gmail.com>
2022-11-13 22:15:11 +0000

    Exercise 2

--- exercises/exercise-2/exercise-2.js
+++ exercises/exercise-2/exercise-2.js
@@ -70,3 +70,59 @@ let hogwarts = [
     occupation: "Teacher",
   },
 ];
+
+
+//----------TASK 1 ----------
+const gryffindorSchool = hogwarts.filter(
+  (element) => element.house === "Gryffindor"
+);
+
+const [...person] = gryffindorSchool;
+ console.log(`Students and Teacher from "Gryffindor" School are as following`);
+for (let i = 0; i < person.length; i++){
+  console.log(`${person[i].firstName} ${person[i].lastName}`);
+}
+
+
+/*
+function getFnamesLnames(arr) {
+  console.log(`Students and Teacher from "Gryffindor" School are as following`);
+  arr.forEach((element) => {
+    if (element.house === "Gryffindor") {
+      console.log(`${element.firstName} ${element.lastName}`);
+    }
+  });
+}
+
+getFnamesLnames(hogwarts);
+*/
+
+
+
+//----------TASK 2 ----------
+const teacherWithPets = hogwarts.filter(
+  (element) => element.occupation === "Teacher" && element.pet !== null
+);
+
+
+const [...whoHasPet] = teacherWithPets;
+
+for (let i = 0; i < whoHasPet.length; i++) {
+  console.log(`The teacher that has a pet is  ${whoHasPet[i].firstName} ${whoHasPet[i].lastName}`
+  );
+}
+
+
+
+/*
+function getTeacherWithPets(arr) {
+  arr.forEach((element) => {
+    if (element.occupation === "Teacher" && element.pet !== null) {
+      console.log(
+        `The teacher that has a pet is ${element.firstName} ${element.lastName}`
+      );
+    }
+  });
+}
+getTeacherWithPets(hogwarts);
+*/
