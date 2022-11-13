1bf0c1d9195cd5edb21b6afc2556579efd81d489
khmdagal <khmdagal@gmail.com>
2022-11-13 23:02:05 +0000

    Exercise 3

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,3 +6,24 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+const [...orderList] = order;
+
+// This will be the title of each column
+console.log(`QTY\tITEM\tTOTAL`);
+const totalBill = orderList.map(element => {
+ 
+  // this will print out each item quantity and total price based on the quanty for this particular item
+  console.log(
+    `${element.quantity}\t${element.itemName}\t\t${Number(
+      element.unitPrice * element.quantity
+    ).toFixed(2)}`
+  );
+  
+  // this retunrs the total for each item and then we add together to get the total bill
+  return element.unitPrice * element.quantity
+}).reduce((a, b) => a + b)
+console.log(`Total: ${totalBill} 
+Enjoy your meal🥗`)
+
+// couldn't aling the Sausage McMuffin price with the other pricess
